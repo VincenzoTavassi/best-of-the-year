12750ccc34239a6869d2038834953b8e014741eb
VincenzoTavassi <info@egadivacanze.it>
2023-06-21 09:46:05 +0200

    step 2 - add requestmapping

--- src/main/java/com/excercises/bestoftheyear/HomeController.java
+++ src/main/java/com/excercises/bestoftheyear/HomeController.java
@@ -3,11 +3,13 @@ package com.excercises.bestoftheyear;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping("/")
